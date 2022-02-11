35f90881d10024246cbbe23efff70094283fe268
Pawnchow <koreshok932@gmail.com>
2022-02-11 19:55:20 +0500

    feature: added features 4-7

--- script/index.js
+++ script/index.js
@@ -1,11 +1,11 @@
 const editButtonProfile = document.querySelector(".profile__edit-button");
-const popup = document.querySelector(".popup");
 const popupProfile = document.querySelector(".popup_profile");
 const popupPhoto = document.querySelector(".popup_photo");
 const profileName = document.querySelector(".profile__name");
 const profileAbout = document.querySelector(".profile__user-text");
 const closeButtonProfile = document.querySelector(".popup__close-button_profile");
 const closeButtonPhoto = document.querySelector(".popup__close-button_photo");
+const closeButtonPhotoFull = document.querySelector(".popup__close-button_full");
 const popupFormProfile = document.querySelector(".popup__form_profile");
 const popupFormPhoto = document.querySelector(".popup__form_add-photo");
 const inputName = document.querySelector('input[name="editName"]');
@@ -13,42 +13,9 @@ const inputAbout = document.querySelector('input[name="editAbout"]');
 const addButton = document.querySelector(".profile__add-button");
 const photoTemplate = document.querySelector(".photo-template");
 const photoItems = document.querySelector(".photo__items");
-
-
- /*function openPopup() {
-  popup.classList.add(" ");
-  inputName.value = profileName.textContent;
-  inputAbout.value = profileAbout.textContent;
-}
-*/
-/*
-function openPopup() {
-  popup.classList.add("popup_opened");
-}
-function closePopup() {
-  popup.classList.remove("popup_opened");
-}
-function openPopupProfile() {
-  inputName.value = profileName.textContent;
-  inputAbout.value = profileAbout.textContent;
-  openPopup();
-}
-
-function handleProfileFormSubmit(event) {
-  event.preventDefault();
-  profileName.textContent = inputName.value;
-  profileAbout.textContent = inputAbout.value;
-  closePopup();
-}
-
-popupForm.addEventListener("submit", handleProfileFormSubmit);
-editButton.addEventListener("click", openPopupProfile);
-closeButton.addEventListener("click", closePopup);
-
-
-addButton.addEventListener("click", document.querySelector(".popup_photo").openPopup);
-
-*/
+const popupPhotoFull = document.querySelector(".popup__full");
+const photoFullImg = document.querySelector(".popup__full_img");
+const photoFullText = document.querySelector(".popup__full_text");
 
 // Initial cards
 const initialCards = [
@@ -94,8 +61,8 @@ function handleProfileFormSubmit(event) {
 };
 
 // Лайк и отмена лайка
-function toggleLike (target) {
-  target.classList.toggle("photo__like_active");
+function toggleLike(event) {
+  event.target.classList.toggle("photo__like_active");
 };
 
 // Добавление карточки в начало
@@ -103,21 +70,34 @@ function addPhoto(item) {
   photoItems.prepend(item);
 };
 
+// Удаление карточки
+function removePhoto(event) {
+  event.target.closest('.photo__item').remove();
+}
+
 // Рендер карточек с фотографиями
 function renderPhotoCard(name, link) {
   const newPhoto = photoTemplate.content.cloneNode(true);
   const photoImg = newPhoto.querySelector(".photo__img");
   const photoText = newPhoto.querySelector(".photo__text");
-  const photoLike = newPhoto.querySelector(".photo__like");
+  const photoLikeBtn = newPhoto.querySelector(".photo__like");
+  const photoRemoveBtn = newPhoto.querySelector(".photo__remove");
   photoImg.src = link;
   photoImg.alt = name;
   photoText.textContent = name;
-  photoLike.addEventListener("click", toggleLike);
+  photoLikeBtn.addEventListener("click", toggleLike);
+  photoRemoveBtn.addEventListener("click", removePhoto);
+  photoImg.addEventListener("click", function () {
+    photoFullImg.src = link;
+    photoFullImg.alt = name;
+    photoFullText.textContent = name;
+    openPopup(popupPhotoFull);
+  });
   return newPhoto;
 };
 
 // Наполнение страницы стартовыми карточками
-initialCards.forEach(function(item) {
+initialCards.forEach(function (item) {
   addPhoto(renderPhotoCard(item.name, item.link));
 });
 
@@ -129,27 +109,27 @@ function handlePhotoFormSubmit(event) {
   popupFormPhoto.reset();
 };
 
-
 // Обработчики попапа редактирования профиля
-editButtonProfile.addEventListener("click", function() {
+editButtonProfile.addEventListener("click", function () {
   inputName.value = profileName.textContent;
   inputAbout.value = profileAbout.textContent;
   openPopup(popupProfile);
 });
-closeButtonProfile.addEventListener("click", function() {
+closeButtonProfile.addEventListener("click", function () {
   closePopup(popupProfile);
 });
 popupFormProfile.addEventListener("submit", handleProfileFormSubmit);
 
-
-
 // Обработчики попапа добавления фотографии
-addButton.addEventListener("click", function() {
+addButton.addEventListener("click", function () {
   openPopup(popupPhoto);
 });
-closeButtonPhoto.addEventListener("click", function() {
+closeButtonPhoto.addEventListener("click", function () {
   closePopup(popupPhoto);
 });
 popupFormPhoto.addEventListener("submit", handlePhotoFormSubmit);
 
-
+// Закрытие попапа с полной фотографией
+closeButtonPhotoFull.addEventListener("click", function () {
+  closePopup(popupPhotoFull);
+});
